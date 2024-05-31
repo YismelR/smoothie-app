8e44f9a8463e3021c707723dcc96b838d4383ac4
YismelR <yismel_rodriguez@outlook.com>
2024-05-31 15:35:14 -0400

    updated the text color on the description and the height of the footer image

--- src/routes/HomeFooter.tsx
+++ src/routes/HomeFooter.tsx
@@ -24,7 +24,7 @@ export default function HomeFooter() {
             <img
               src={smoothie.src}
               alt={smoothie.alt}
-              className={`md-tablet:h-28 s-laptop:-top-12 md-desktop:h-32 absolute md-desktop:-top-16 s-laptop:left-6 rounded-full border-2 ${smoothie.borderColor} md-desktop:left-8 lg-desktop:left-12 lg-desktop:w-48 lg-desktop:h-48 lg-desktop:-top-32 lg-desktop:border-4 md-tablet:-top-16 md-tablet:left-8 lg-phone:h-20 lg-phone:left-3 lg-phone:-top-8 ${smoothie.shadowColor} md-phone:h-16 s-laptop:h-24 s-phone:h-16`}
+              className={`md-tablet:h-28 s-laptop:-top-12 md-desktop:h-32 absolute md-desktop:-top-20 s-laptop:left-6 rounded-full border-2 ${smoothie.borderColor} md-desktop:left-8 lg-desktop:left-12 lg-desktop:w-48 lg-desktop:h-48 lg-desktop:-top-24 lg-desktop:border-4 md-tablet:-top-16 md-tablet:left-8 lg-phone:h-20 lg-phone:left-3 lg-phone:-top-8 ${smoothie.shadowColor} md-phone:h-16 s-laptop:h-24 s-phone:h-16`}
             />
             <p
               className={`text-center font-semibold hidden ${smoothie.className} md-tablet:flex`}
