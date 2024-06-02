a32786c6dca293c788a533344d700806f2180bde
YismelR <yismel_rodriguez@outlook.com>
2024-06-02 19:52:24 -0400

    Added some test, for the Home, Bowls, About and contact us link. need apply the test for order now and more

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,20 +1,12 @@
 // import App from "@/routes/App";
+import App from "@/routes/home/App";
 import HomeFooter from "@/routes/home/HomeFooter";
-import { render } from "@testing-library/react";
-// import { createMemoryRouter } from "react-router-dom";
-import { expect, test } from "vitest";
-// import userEvent from "@testing-library/user-event";
-
-// Create In memory router
-
-// describe("A truthy statement", () => {
-//   const router = createMemoryRouter([
-//     {
-//       path: "/",
-//       element: <App />,
-//     },
-//   ]);
-// });
+import { fireEvent, getByText, render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { expect, test, vi } from "vitest";
+import userEvent from "@testing-library/user-event";
+import HomeDescription from "@/routes/home/HomeDescription";
+import HomeImageMore from "@/routes/home/HomeImageMore";
 
 test("the smoothie card", async () => {
   const wrapper = render(<HomeFooter />);
@@ -28,7 +20,89 @@ test("smoothie card text", async () => {
     "Blueberry Banana Smoothie".toLowerCase()
   );
 });
-// test("smoothie card clicked", async () => {
-//   const wrapper = render(<HomeFooter />);
-//   const user = userEvent.setup();
-// });
+
+test("the 'order now' button click", async () => {
+  const wrapper = render(<HomeDescription />);
+  const button = wrapper.container.querySelector("button") as HTMLButtonElement;
+
+  expect(button.textContent).toBe("Order Now");
+
+  fireEvent(
+    getByText(button, "Order Now"),
+    new MouseEvent("click", {
+      bubbles: true,
+    })
+  );
+
+  //expect the order now button to be the order Now page description
+});
+
+test("the 'more' button click", async () => {
+  const wrapper = render(<HomeImageMore />);
+  const button = wrapper.container.querySelector("button") as HTMLButtonElement;
+
+  expect(button.textContent).toBe("More");
+
+  fireEvent(
+    getByText(button, "More"),
+    new MouseEvent("click", {
+      bubbles: true,
+    })
+  );
+
+  //expect the More button to be the More details page
+});
+
+test("Home router link receives atleast one click", async () => {
+  render(<App />, { wrapper: BrowserRouter });
+
+  expect(screen.getByText("Home")).toBeInTheDocument();
+
+  const user = userEvent.setup();
+  const home = vi.spyOn(user, "click");
+
+  const homeLink = screen.getByText(/Home/i);
+
+  await user.click(homeLink);
+  expect(home).toHaveBeenCalledTimes(1);
+});
+
+test("Bowls router link receives atleast one click", async () => {
+  render(<App />, { wrapper: BrowserRouter });
+
+  expect(screen.getByText("Bowls")).toBeInTheDocument();
+
+  const user = userEvent.setup();
+  const bowls = vi.spyOn(user, "click");
+  const bowlsLink = screen.getByText(/Bowls/i);
+
+  await user.click(bowlsLink);
+
+  expect(bowls).toHaveBeenCalledTimes(1);
+});
+
+test("About router link receives atleast one click", async () => {
+  render(<App />, { wrapper: BrowserRouter });
+
+  expect(screen.getByText("About")).toBeInTheDocument();
+
+  const user = userEvent.setup();
+  const about = vi.spyOn(user, "click");
+  const aboutLink = screen.getByText(/About/i);
+
+  await user.click(aboutLink);
+  expect(about).toHaveBeenCalledTimes(1);
+});
+
+test("Contact Us router link receives atleast one click", async () => {
+  render(<App />, { wrapper: BrowserRouter });
+
+  expect(screen.getByText("Contact Us")).toBeInTheDocument();
+
+  const user = userEvent.setup();
+  const contactUs = vi.spyOn(user, "click");
+  const contactUsLink = screen.getByText(/Contact Us/i);
+
+  await user.click(contactUsLink);
+  expect(contactUs).toHaveBeenCalledTimes(1);
+});
