236761b35a744152b06d53c7006427a73b5977b9
YismelR <yismel_rodriguez@outlook.com>
2024-06-25 12:05:51 -0400

    responsive layout for the about page

--- src/routes/about/About.tsx
+++ src/routes/about/About.tsx
@@ -9,14 +9,14 @@ export default function About() {
   return (
     <>
       <div className="s-phone:grid s-phone:gap-8 s-phone:px-4 md-tablet:px-12 md-laptop:px-32">
-        <div className="md-tablet:grid md-tablet:place-items-center ">
+        <div className="md-tablet:grid md-tablet:place-items-center md-laptop:gap-4 lg-desktop:gap-0">
           <h1 className=" s-phone:text-2xl s-phone:flex s-phone:justify-center s-phone:my-8 s-phone:font-bold md-tablet:text-3xl md-tablet:mb-8 md-desktop:text-4xl lg-desktop:text-5xl lg-desktop:my-20">
             <div className="relative overflow-hidden lg-desktop:pb-4 s-phone:pb-2">
               About
               <span className="absolute left-0 bottom-0 h-0.5 bg-gradient-to-r from-purple-500 to-pink-500 animate-underline"></span>
             </div>
           </h1>
-          <p className=" text-center s-phone:text-sm lg-phone:text-base md-tablet:px-16 md-tablet:max-w-3xl">
+          <p className=" text-center s-phone:text-sm lg-phone:text-base md-tablet:px-16 md-tablet:max-w-3xl md-desktop:text-xl lg-desktop:text-3xl lg-desktop:p-0">
             Welcome to Smoothie: Smoothie Bowls Edition, your go-to app for
             crafting the perfect smoothie bowl experience. Whether you’re a
             health enthusiast, a culinary adventurer, or someone who loves a
@@ -26,10 +26,10 @@ export default function About() {
         </div>
         <div className="s-phone:grid s-phone:place-items-center md-tablet:grid-cols-2">
           <div className="s-phone:grid s-phone:gap-4 s-phone:place-items-center md-tablet:place-items-start">
-            <h2 className="s-phone:text-xl font-semibold md-tablet:text-2xl">
+            <h2 className="s-phone:text-xl font-semibold md-tablet:text-2xl md-desktop:text-3xl lg-desktop:text-4xl">
               Our Mission
             </h2>
-            <p className="s-phone:text-sm lg-phone:text-base">
+            <p className="s-phone:text-sm lg-phone:text-base md-desktop:text-xl lg-desktop:text-3xl">
               At Smoothie, our mission is to inspire a healthier lifestyle by
               making it easy and fun to create delicious, nutrient-packed
               smoothie bowls. We believe that everyone deserves to enjoy the
@@ -43,13 +43,13 @@ export default function About() {
             className="row-start-1 md-tablet:col-start-2 "
           />
         </div>
-        <div className="s-phone:grid s-phone:gap-4 md-tablet:grid-cols-2 md-tablet:place-items-center md-tablet:gap-8">
+        <div className="s-phone:grid s-phone:gap-4 md-tablet:grid-cols-2 md-tablet:place-items-center md-tablet:gap-8 md-laptop:gap-12 md-desktop:gap-16">
           <img src={bowls} alt="bowls" />
           <div className="s-phone:grid s-phone:gap-4 s-phone:place-items-center md-tablet:place-items-start">
-            <h2 className="s-phone:text-xl font-semibold md-tablet:text-2xl">
+            <h2 className="s-phone:text-xl font-semibold md-tablet:text-2xl md-desktop:text-3xl lg-desktop:text-4xl">
               Why Smoothie Bowls?
             </h2>
-            <p className="s-phone:text-sm lg-phone:text-base">
+            <p className="s-phone:text-sm lg-phone:text-base md-desktop:text-xl lg-desktop:text-3xl">
               Smoothie bowls are not only visually appealing but also a
               fantastic way to pack a variety of nutrients into a satisfying
               meal. They help you increase your intake of fruits and vegetables,
@@ -65,11 +65,11 @@ export default function About() {
         className={`${selectedSmoothie.backgroundColor} bg-opacity-5 s-phone:grid s-phone:gap-4 s-phone:place-items-center s-phone:mt-16 s-phone:px-4 s-phone:py-8 s-phone:pb-8 md-tablet:px-12 md-tablet:py-16`}
       >
         <h2
-          className={`${selectedSmoothie.textColor} s-phone:text-xl font-semibold md-tablet:text-2xl`}
+          className={`${selectedSmoothie.textColor} s-phone:text-xl font-semibold md-tablet:text-2xl md-desktop:text-3xl lg-desktop:text-4xl`}
         >
           Join Us
         </h2>
-        <p className="text-center s-phone:text-sm lg-phone:text-base md-tablet:px-16 md-tablet:max-w-3xl">
+        <p className="text-center s-phone:text-sm lg-phone:text-base md-tablet:px-16 md-tablet:max-w-3xl md-desktop:text-xl lg-desktop:text-3xl lg-desktop:p-0">
           Join the Smoothie community today and start your journey towards a
           healthier, happier you. Explore our recipes, and blend your way to
           wellness with stunning smoothie bowls. Embrace the well-crafted
--- src/routes/about/Features.tsx
+++ src/routes/about/Features.tsx
@@ -1,40 +1,46 @@
 export function Features() {
   return (
     <div className="s-phone:grid s-phone:gap-4 s-phone:place-items-center md-tablet:gap-8 md-tablet:place-items-start">
-      <h2 className="s-phone:text-xl font-semibold md-tablet:text-2xl">
+      <h2 className="s-phone:text-xl font-semibold md-tablet:text-2xl md-desktop:text-3xl lg-desktop:text-4xl">
         Features
       </h2>
-      <div className="s-phone:grid s-phone:gap-4 md-phone:gap-8 md-tablet:grid-cols-2">
-        <div className="s-phone:grid s-phone:gap-4 bg-grey-lightCard s-phone:p-4 s-phone:rounded-2xl shadow-cardShadow">
-          <h3 className="font-medium text-lg">Customizable Recipes</h3>
-          <p className="s-phone:text-sm lg-phone:text-base">
+      <div className="s-phone:grid s-phone:gap-4 md-phone:gap-8 md-tablet:grid-cols-2 md-laptop:gap-12 md-desktop:gap-16">
+        <div className="s-phone:grid s-phone:gap-4 bg-grey-lightCard s-phone:p-4 s-phone:rounded-2xl shadow-cardShadow md-laptop:p-12">
+          <h3 className="font-medium text-lg md-desktop:text-xl lg-desktop:text-3xl">
+            Customizable Recipes
+          </h3>
+          <p className="s-phone:text-sm lg-phone:text-base md-desktop:text-lg lg-desktop:text-2xl">
             Tailor your smoothie bowls to your taste and dietary preferences
             with our extensive library of customizable recipes. Choose from a
             variety of fruits, vegetables, superfoods, nuts, seeds, and granola
             options to create your perfect blend.
           </p>
         </div>
-        <div className="s-phone:grid s-phone:gap-4 bg-grey-lightCard s-phone:p-4 s-phone:rounded-2xl shadow-cardShadow">
-          <h3 className="font-medium text-lg">Shopping List</h3>
-          <p className="s-phone:text-sm lg-phone:text-base">
+        <div className="s-phone:grid s-phone:gap-4 bg-grey-lightCard s-phone:p-4 s-phone:rounded-2xl shadow-cardShadow md-laptop:p-12">
+          <h3 className="font-medium text-lg md-desktop:text-xl lg-desktop:text-3xl">
+            Shopping List
+          </h3>
+          <p className="s-phone:text-sm lg-phone:text-base md-desktop:text-lg lg-desktop:text-2xl">
             Simplify your grocery shopping with our integrated shopping list
             feature. Add ingredients from your favorite recipes with a single
             tap, and never miss an essential item again.
           </p>
         </div>
-        <div className="s-phone:grid s-phone:gap-4 bg-grey-lightCard s-phone:p-4 s-phone:rounded-2xl shadow-cardShadow">
-          <h3 className="font-medium text-lg">Nutritional Information</h3>
-          <p className="s-phone:text-sm lg-phone:text-base">
+        <div className="s-phone:grid s-phone:gap-4 bg-grey-lightCard s-phone:p-4 s-phone:rounded-2xl shadow-cardShadow md-laptop:p-12">
+          <h3 className="font-medium text-lg md-desktop:text-xl lg-desktop:text-3xl">
+            Nutritional Information
+          </h3>
+          <p className="s-phone:text-sm lg-phone:text-base md-desktop:text-lg lg-desktop:text-2xl">
             Stay informed about what you’re consuming with detailed nutritional
             breakdowns for each recipe. Track your intake of calories, vitamins,
             minerals, and macronutrients effortlessly.
           </p>
         </div>
-        <div className="s-phone:grid s-phone:gap-4 bg-grey-lightCard s-phone:p-4 s-phone:rounded-2xl shadow-cardShadow">
-          <h3 className="font-medium text-lg">
+        <div className="s-phone:grid s-phone:gap-4 bg-grey-lightCard s-phone:p-4 s-phone:rounded-2xl shadow-cardShadow md-laptop:p-12">
+          <h3 className="font-medium text-lg md-desktop:text-xl lg-desktop:text-3xl">
             Seasonal and Local Ingredients
           </h3>
-          <p className="s-phone:text-sm lg-phone:text-base">
+          <p className="s-phone:text-sm lg-phone:text-base md-desktop:text-lg lg-desktop:text-2xl">
             Enjoy the freshest ingredients by accessing recipes that highlight
             seasonal and locally sourced produce. Support local farmers and
             reduce your carbon footprint while enjoying peak flavor and
