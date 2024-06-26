9dd4879a6f7b87988f828989ebd34e5aee691bde
YismelR <yismel_rodriguez@outlook.com>
2024-06-26 19:10:57 -0400

    updated the drawer dialog for the larger screen

--- src/routes/smoothie-details/DrawerDialog.tsx
+++ src/routes/smoothie-details/DrawerDialog.tsx
@@ -38,17 +38,19 @@ export function DrawerDialog() {
         <DialogTrigger asChild>
           <Button
             variant="outline"
-            className={`text-white ${smoothie.hoverColor}  ${smoothie.backgroundColor} px-4 py-2 rounded-[2rem] cursor-pointer lg-phone:text-xl md-tablet:text-2xl s-laptop:text-xl md-laptop:text-2xl lg-desktop:text-4xl lg-desktop:py-8`}
+            className={`text-white ${smoothie.hoverColor}  ${smoothie.backgroundColor} px-4 py-2 rounded-[2rem] cursor-pointer lg-phone:text-xl md-tablet:text-2xl s-laptop:text-xl md-laptop:text-2xl md-desktop:py-7 lg-desktop:text-4xl lg-desktop:py-8`}
           >
             Add To Cart
           </Button>
         </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px] ">
+        <DialogContent className="sm:max-w-[425px] gap-6 lg-desktop:gap-8">
           <DialogHeader className=" flex flex-row gap-4">
             <img src={addedCart} alt="added to cart" className="w-10" />
             <div className="flex flex-col">
-              <DialogTitle>1 Item Added to the Cart</DialogTitle>
-              <DialogDescription>
+              <DialogTitle className="lg-desktop:text-3xl">
+                1 Item Added to the Cart
+              </DialogTitle>
+              <DialogDescription className="lg-desktop:text-2xl">
                 (2) items in the cart for $40.99
               </DialogDescription>
             </div>
@@ -56,7 +58,11 @@ export function DrawerDialog() {
           <ProfileForm />
           <DialogFooter className="sm:justify-start">
             <DialogClose asChild>
-              <Button type="button" variant="secondary">
+              <Button
+                type="button"
+                variant="outline"
+                className="lg-desktop:text-2xl"
+              >
                 Continue Shopping
               </Button>
             </DialogClose>
@@ -101,18 +107,31 @@ function ProfileForm({ className }: React.ComponentProps<"form">) {
   const smoothie = useLoaderData() as Smoothie;
 
   return (
-    <form className={cn("grid items-start gap-4", className)}>
-      <div className="flex gap-4">
-        <img src={smoothie.src} alt={smoothie.alt} className="max-w-28" />
-        <div className="flex flex-col gap-4 justify-center">
-          <p className="font-semibold">{smoothie.text}</p>
-          <p>${smoothie.price}</p>
+    <form
+      className={cn(
+        "grid items-start gap-4 md-tablet:gap-6 lg-desktop:gap-8",
+        className
+      )}
+    >
+      <div className="flex gap-4 lg-desktop:gap-8">
+        <img
+          src={smoothie.src}
+          alt={smoothie.alt}
+          className="max-w-28 md-tablet:max-w-36 lg-desktop:max-w-44"
+        />
+        <div className="flex flex-col gap-4 justify-center md-tablet:justify-start lg-desktop:justify-center">
+          <p className="font-semibold md-tablet:text-2xl lg-desktop:text-3xl">
+            {smoothie.text}
+          </p>
+          <p className="md-tablet:text-xl lg-desktop:text-3xl">
+            ${smoothie.price}
+          </p>
         </div>
       </div>
       <Link to={"/shopping-cart"} className="flex">
         <Button
           type="submit"
-          className={`text-white w-full ${smoothie.hoverColor}  ${smoothie.backgroundColor} px-4 py-2 rounded-[2rem] cursor-pointer lg-phone:text-xl md-tablet:text-2xl s-laptop:text-xl md-laptop:text-2xl lg-desktop:text-4xl lg-desktop:py-4`}
+          className={`text-white w-full ${smoothie.hoverColor}  ${smoothie.backgroundColor} px-4 py-2 rounded-[2rem] cursor-pointer lg-phone:text-xl md-tablet:text-2xl md-tablet:py-6 s-laptop:text-xl md-laptop:text-2xl lg-desktop:text-4xl lg-desktop:py-7`}
         >
           View Cart (2)
         </Button>
