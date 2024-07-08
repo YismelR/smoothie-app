d3b703717adcbf2974f83bfb813560442f9749f1
YismelR <yismel_rodriguez@outlook.com>
2024-07-08 14:29:01 -0400

    fixed the height screen size when there's no item in the favorite list

--- src/features/bowls/Bowls.tsx
+++ src/features/bowls/Bowls.tsx
@@ -1,9 +1,29 @@
+import useSmoothiesStore from "@/store/store";
 import BowlsList from "./BowlsList";
 import Favorite from "./Favorite";
 
 export default function Bowls() {
+  const { currentFilter, favoriteSmoothiesList } = useSmoothiesStore(
+    (state) => state
+  );
+  const heightScreen =
+    currentFilter === "favorite" && favoriteSmoothiesList.length === 0
+      ? "h-screen"
+      : "";
+
+  function noSmoothieFavorited() {
+    if (currentFilter === "favorite" && favoriteSmoothiesList.length === 0) {
+      return (
+        <div className="flex justify-center">
+          <h1 className="font-medium">
+            YOU HAVE NOT ADDED ANY BOWLS TO YOUR FAVORITE
+          </h1>
+        </div>
+      );
+    }
+  }
   return (
-    <div className="s-phone:pb-16 lg-desktop:pb-28">
+    <div className={`s-phone:pb-16 lg-desktop:pb-28 ${heightScreen}`}>
       <h1 className=" s-phone:text-2xl s-phone:flex s-phone:justify-center s-phone:my-8 s-phone:font-bold md-tablet:text-3xl md-tablet:mb-10 md-desktop:text-4xl lg-desktop:text-5xl lg-desktop:my-20">
         <div className="relative overflow-hidden lg-desktop:pb-4 s-phone:pb-2">
           Bowls
@@ -11,6 +31,7 @@ export default function Bowls() {
         </div>
       </h1>
       <Favorite />
+      {noSmoothieFavorited()}
       <BowlsList />
     </div>
   );
