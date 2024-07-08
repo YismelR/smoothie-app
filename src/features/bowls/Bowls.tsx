38ab4687325f55d5cc340543c7464a4cdc59279a
YismelR <yismel_rodriguez@outlook.com>
2024-07-08 15:51:33 -0400

    added a favorite bowl image to display when no favorite has been added

--- src/features/bowls/Bowls.tsx
+++ src/features/bowls/Bowls.tsx
@@ -1,6 +1,7 @@
 import useSmoothiesStore from "@/store/store";
 import BowlsList from "./BowlsList";
 import Favorite from "./Favorite";
+import notInFavorite from "@/assets/icons/not-added-favorite.svg";
 
 export default function Bowls() {
   const { currentFilter, favoriteSmoothiesList } = useSmoothiesStore(
@@ -14,10 +15,11 @@ export default function Bowls() {
   function noSmoothieFavorited() {
     if (currentFilter === "favorite" && favoriteSmoothiesList.length === 0) {
       return (
-        <div className="flex justify-center">
-          <h1 className="font-medium">
-            YOU HAVE NOT ADDED ANY BOWLS TO YOUR FAVORITE
+        <div className="flex flex-col place-items-center gap-8 mt-12">
+          <h1 className="font-semibold text-center px-4 md-desktop:text-xl lg-desktop:text-3xl">
+            YOUR FAVORITE BOWLS LIST IS EMPTY. WHY NOT ADD SOME NOW?
           </h1>
+          <img src={notInFavorite} alt="not in favorite" className="size-24" />
         </div>
       );
     }
