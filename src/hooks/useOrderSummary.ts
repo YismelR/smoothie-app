796f93c0e86c50ca3b0b66c5fa0a1b1e95de3680
YismelR <yismel_rodriguez@outlook.com>
2024-07-16 20:01:50 -0400

    updated the order summary calculation

--- src/hooks/useOrderSummary.ts
+++ src/hooks/useOrderSummary.ts
@@ -3,14 +3,14 @@ import useShoppingCartStore from "@/store/cartConfirmation";
 export function useOrderSummary() {
   const totalAmount = useShoppingCartStore((state) => state.totalAmount);
 
-  const shippingCost = 1.75;
+  const shippingCost = totalAmount > 0 ? 1.75 : 0;
   const totalAmountItem = totalAmount;
   const totalAmountItemDisplay = totalAmountItem.toFixed(2);
 
   const totalBeforeTax = totalAmountItem + shippingCost;
   const totalBeforeTaxDisplay = totalBeforeTax.toFixed(2);
 
-  const taxTotal = 0.08875 * totalBeforeTax;
+  const taxTotal = totalAmount > 0 ? 0.08875 * totalBeforeTax : 0;
   const tax = taxTotal.toFixed(2);
 
   const totalAmountAfter = totalBeforeTax + taxTotal;
