81252e5b7dff1f311c634a6fde39715183ed4c6f
YismelR <yismel_rodriguez@outlook.com>
2024-07-18 19:19:07 -0400

    added prettier-plugin-tailwindcss

--- src/features/checkout/summary/CheckoutSummary.tsx
+++ src/features/checkout/summary/CheckoutSummary.tsx
@@ -6,8 +6,8 @@ import SummaryButton from "./SummaryButton";
 
 export default function CheckoutSummary() {
   return (
-    <div className="flex flex-col md-tablet:pb-8 md-tablet:grid md-tablet:grid-cols-2 md-tablet:px-12 md-tablet:gap-4 s-laptop:p-12 md-laptop:px-32 justify-center md-laptop:gap-4 md-desktop:gap-8 md-laptop:grid-cols-3">
-      <div className="flex flex-col gap-4 mt-8 md-tablet:mt-0 md-tablet:gap-8 md-laptop:col-span-2">
+    <div className="flex flex-col justify-center md-tablet:grid md-tablet:grid-cols-2 md-tablet:gap-4 md-tablet:px-12 md-tablet:pb-8 s-laptop:p-12 md-laptop:grid-cols-3 md-laptop:gap-4 md-laptop:px-32 md-desktop:gap-8">
+      <div className="mt-8 flex flex-col gap-4 md-tablet:mt-0 md-tablet:gap-8 md-laptop:col-span-2">
         <DeliveryTo />
         <PayingWith />
         <ItemsTableSummary />
