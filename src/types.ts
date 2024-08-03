4f75c5c76ce33d515514234aa8d9b17f35d668d1
YismelR <yismel_rodriguez@outlook.com>
2024-08-02 23:22:22 -0400

    fixed the type smoothie

--- src/types.ts
+++ src/types.ts
@@ -10,7 +10,7 @@ export type Smoothie = {
   mainBackgroundColor: string;
   bgFill: string;
   logoFill: string;
-  shadowBtnColor: String;
+  shadowBtnColor: string;
   text: string;
   textColor: string;
   className: string;
