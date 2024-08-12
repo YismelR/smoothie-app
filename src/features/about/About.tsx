a61736467013464918f02de2c1f5c33c378d6393
YismelR <yismel_rodriguez@outlook.com>
2024-08-12 12:39:25 -0400

    applied the skeleton for the images in the about page, created a higher order component called imageskeleton which handles the images on loading

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -2,21 +2,22 @@ import fruits from "@/assets/images/fruits-about.svg";
 import bowls from "@/assets/images/bowls-about.svg";
 import { Features } from "./Features";
 import useSmoothiesStore from "@/store/store";
+import ImageSkeleton from "./ImageSkeleton";
 
 export default function About() {
   const selectedSmoothie = useSmoothiesStore((state) => state.selectedSmoothie);
 
   return (
     <>
-      <div className="s-phone:grid s-phone:gap-8 s-phone:px-4 md-tablet:px-12 md-laptop:px-32">
+      <div className="s-phone:grid s-phone:gap-8 s-phone:px-4 md-tablet:px-12 md-laptop:px-32 lg-desktop:gap-16">
         <div className="md-tablet:grid md-tablet:place-items-center md-laptop:gap-4 lg-desktop:gap-0">
-          <h1 className=" s-phone:text-2xl s-phone:flex s-phone:justify-center s-phone:my-8 s-phone:font-bold md-tablet:text-3xl md-tablet:mb-8 md-desktop:text-4xl lg-desktop:text-5xl lg-desktop:my-20">
-            <div className="relative overflow-hidden lg-desktop:pb-4 s-phone:pb-2">
+          <h1 className="s-phone:my-8 s-phone:flex s-phone:justify-center s-phone:text-2xl s-phone:font-bold md-tablet:mb-8 md-tablet:text-3xl md-desktop:text-4xl lg-desktop:my-20 lg-desktop:text-5xl">
+            <div className="relative overflow-hidden s-phone:pb-2 lg-desktop:pb-4">
               About
-              <span className="absolute left-0 bottom-0 h-0.5 bg-gradient-to-r from-purple-500 to-pink-500 animate-underline"></span>
+              <span className="absolute bottom-0 left-0 h-0.5 animate-underline bg-gradient-to-r from-purple-500 to-pink-500"></span>
             </div>
           </h1>
-          <p className=" text-center s-phone:text-sm lg-phone:text-base md-tablet:px-16 md-tablet:max-w-3xl md-desktop:text-xl lg-desktop:text-3xl lg-desktop:p-0">
+          <p className="text-center s-phone:text-sm lg-phone:text-base md-tablet:max-w-3xl md-tablet:px-16 md-desktop:text-xl lg-desktop:p-0 lg-desktop:text-3xl">
             Welcome to Smoothie: Smoothie Bowls Edition, your go-to app for
             crafting the perfect smoothie bowl experience. Whether you’re a
             health enthusiast, a culinary adventurer, or someone who loves a
@@ -24,9 +25,9 @@ export default function About() {
             smoothie bowl creations easier and more enjoyable.
           </p>
         </div>
-        <div className="s-phone:grid s-phone:place-items-center md-tablet:grid-cols-2">
-          <div className="s-phone:grid s-phone:gap-4 s-phone:place-items-center md-tablet:place-items-start">
-            <h2 className="s-phone:text-xl font-semibold md-tablet:text-2xl md-desktop:text-3xl lg-desktop:text-4xl">
+        <div className="s-phone:grid s-phone:place-items-center md-tablet:grid-cols-2 md-tablet:gap-4">
+          <div className="flex-col s-phone:flex s-phone:place-items-center s-phone:gap-4 md-tablet:place-items-start">
+            <h2 className="font-semibold s-phone:text-xl md-tablet:text-2xl md-desktop:text-3xl lg-desktop:text-4xl">
               Our Mission
             </h2>
             <p className="s-phone:text-sm lg-phone:text-base md-desktop:text-xl lg-desktop:text-3xl">
@@ -37,16 +38,20 @@ export default function About() {
               vibrant, satisfying bowls that nourish both body and soul.
             </p>
           </div>
-          <img
+          <ImageSkeleton
             src={fruits}
             alt="fruits"
-            className="row-start-1 md-tablet:col-start-2 "
+            className="row-start-1 h-[240px] md-tablet:col-start-2 md-laptop:h-[400px] lg-desktop:h-[600px]"
           />
         </div>
         <div className="s-phone:grid s-phone:gap-4 md-tablet:grid-cols-2 md-tablet:place-items-center md-tablet:gap-8 md-laptop:gap-12 md-desktop:gap-16">
-          <img src={bowls} alt="bowls" />
-          <div className="s-phone:grid s-phone:gap-4 s-phone:place-items-center md-tablet:place-items-start">
-            <h2 className="s-phone:text-xl font-semibold md-tablet:text-2xl md-desktop:text-3xl lg-desktop:text-4xl">
+          <ImageSkeleton
+            src={bowls}
+            alt="bowls"
+            className="h-[240px] lg-desktop:h-[400px]"
+          />
+          <div className="s-phone:grid s-phone:place-items-center s-phone:gap-4 md-tablet:place-items-start">
+            <h2 className="font-semibold s-phone:text-xl md-tablet:text-2xl md-desktop:text-3xl lg-desktop:text-4xl">
               Why Smoothie Bowls?
             </h2>
             <p className="s-phone:text-sm lg-phone:text-base md-desktop:text-xl lg-desktop:text-3xl">
@@ -62,14 +67,14 @@ export default function About() {
         <Features />
       </div>
       <div
-        className={`${selectedSmoothie.backgroundColor} bg-opacity-5 s-phone:grid s-phone:gap-4 s-phone:place-items-center s-phone:mt-16 s-phone:px-4 s-phone:py-8 s-phone:pb-8 md-tablet:px-12 md-tablet:py-16`}
+        className={`${selectedSmoothie.backgroundColor} bg-opacity-5 s-phone:mt-16 s-phone:grid s-phone:place-items-center s-phone:gap-4 s-phone:px-4 s-phone:py-8 s-phone:pb-8 md-tablet:px-12 md-tablet:py-16`}
       >
         <h2
-          className={`${selectedSmoothie.textColor} s-phone:text-xl font-semibold md-tablet:text-2xl md-desktop:text-3xl lg-desktop:text-4xl`}
+          className={`${selectedSmoothie.textColor} font-semibold s-phone:text-xl md-tablet:text-2xl md-desktop:text-3xl lg-desktop:text-4xl`}
         >
           Join Us
         </h2>
-        <p className="text-center s-phone:text-sm lg-phone:text-base md-tablet:px-16 md-tablet:max-w-3xl md-desktop:text-xl lg-desktop:text-3xl lg-desktop:p-0">
+        <p className="text-center s-phone:text-sm lg-phone:text-base md-tablet:max-w-3xl md-tablet:px-16 md-desktop:text-xl lg-desktop:p-0 lg-desktop:text-3xl">
           Join the Smoothie community today and start your journey towards a
           healthier, happier you. Explore our recipes, and blend your way to
           wellness with stunning smoothie bowls. Embrace the well-crafted
---
+++ src/features/about/ImageSkeleton.tsx
@@ -0,0 +1,29 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { useState, useEffect } from "react";
+
+type ImageSkeletonProps = {
+  src: string;
+  alt: string;
+  className: string;
+};
+
+function ImageSkeleton({ src, alt, className }: ImageSkeletonProps) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading or fetch your data here
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // Adjust the delay as needed
+
+    return () => clearTimeout(timer); // Cleanup the timeout if the component unmounts
+  }, []);
+
+  if (isLoading) {
+    return <Skeleton className={`${className} w-full`} />;
+  }
+
+  return <img src={src} alt={alt} className={className} />;
+}
+
+export default ImageSkeleton;
