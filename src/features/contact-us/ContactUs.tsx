e9285ec5e9cda222dfd0e446a1fe5765b334fe8b
YismelR <yismel_rodriguez@outlook.com>
2024-08-12 12:51:01 -0400

    fixed the loading state of the images on the contact us using the image skeleton component

--- src/features/contact-us/ContactUs.tsx
+++ src/features/contact-us/ContactUs.tsx
@@ -1,13 +1,14 @@
 import useSmoothiesStore from "@/store/store";
 import { ContactForm } from "./ContactForm";
 import contactImg from "@/assets/images/contact-image.svg";
+import ImageSkeleton from "../about/ImageSkeleton";
 export default function ContactUs() {
   const selectedSmoothie = useSmoothiesStore((state) => state.selectedSmoothie);
   return (
-    <div className=" s-phone:pb-16 lg-desktop:pb-28 s-phone:px-4 s-phone:grid s-phone:gap-4 s-phone:mt-4 md-tablet:place-items-center md-tablet:px-12 s-laptop:grid-cols-2 s-laptop:my-12 md-laptop:pl-32 md-laptop:pr-24 md-desktop:my-32 md-desktop:pr-16 lg-desktop:h-[50rem] lg-desktop:my-0 lg-desktop:mt-24">
-      <div className="s-phone:grid s-phone:gap-4 lg-desktop:h-full lg-desktop:flex lg-desktop:flex-col lg-desktop:justify-center lg-desktop:gap-16">
+    <div className="s-phone:mt-4 s-phone:grid s-phone:gap-4 s-phone:px-4 s-phone:pb-16 md-tablet:place-items-center md-tablet:px-12 s-laptop:my-12 s-laptop:grid-cols-2 md-laptop:pl-32 md-laptop:pr-24 md-desktop:my-32 md-desktop:pr-16 lg-desktop:my-0 lg-desktop:mt-24 lg-desktop:h-[50rem] lg-desktop:pb-28">
+      <div className="s-phone:grid s-phone:gap-4 lg-desktop:flex lg-desktop:h-full lg-desktop:flex-col lg-desktop:justify-center lg-desktop:gap-16">
         <h1
-          className={`${selectedSmoothie.textColor} s-phone:font-semibold s-phone:text-2xl lg-phone:text-3xl md-tablet:text-4xl md-desktop:text-5xl lg-desktop:text-6xl`}
+          className={`${selectedSmoothie.textColor} s-phone:text-2xl s-phone:font-semibold lg-phone:text-3xl md-tablet:text-4xl md-desktop:text-5xl lg-desktop:text-6xl`}
         >
           Contact Us
         </h1>
@@ -17,10 +18,10 @@ export default function ContactUs() {
         </p>
         <ContactForm />
       </div>
-      <img
+      <ImageSkeleton
         src={contactImg}
-        alt="contact us"
-        className="s-phone:row-start-1 s-laptop:col-start-2 lg-desktop:h-full"
+        alt="contactImg"
+        className="row-start-1 h-[204px] place-self-center md-tablet:h-[404px] s-laptop:col-start-2 md-laptop:h-[424px] md-desktop:h-[504px] lg-desktop:h-[624px]"
       />
     </div>
   );
